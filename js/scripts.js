639bbe6b42208c755665a1e3be0cd7be235a761b
Pablo Lara Roloff <pablo.lara.roloff@gmail.com>
2023-07-28 14:50:02 +0200

    Add initial Pokedex data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-alert("Hello world");
+const pokemonList = [];
 
-const favoriteFood = "Pizza";
-document.write(favoriteFood);
+const pokemon1 = {
+  name: "Bulbasaur",
+  height: 7,
+  weight: 6.9,
+  types: ["grass", "poison"],
+};
+
+const pokemon2 = {
+  name: "Charmander",
+  height: 6,
+  weight: 9,
+  types: ["fire"],
+};
+
+const pokemon3 = {
+  name: "Squirtle",
+  height: 0.5,
+  weight: 8.5,
+  types: ["water"],
+};
+
+// Add the Pokémon objects to the pokemonList array
+pokemonList.push(pokemon1, pokemon2, pokemon3);
