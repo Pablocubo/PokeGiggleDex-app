ddf4134b8c33f0b8948b8c448cc7c1f4d576ce69
Pablo Lara Roloff <pablo.lara.roloff@gmail.com>
2023-07-31 13:49:06 +0200

    added css, added Loop in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 const pokemonList = [];
 
+// This is a Object
 const pokemon1 = {
-  name: "Bulbasaur",
-  height: 7,
-  weight: 6.9,
-  types: ["grass", "poison"],
+  name: "Bulbasaur", // <--- This is a String
+  height: 7, // <--- This is a Number
+  weight: 6.9, // weight is a variable and 6.9 is a Value
+  types: ["grass", "poison"], // <--- This is a Array
 };
 
 const pokemon2 = {
@@ -23,3 +24,22 @@ const pokemon3 = {
 
 // Add the Pokémon objects to the pokemonList array
 pokemonList.push(pokemon1, pokemon2, pokemon3);
+
+for (let i = 0; i < pokemonList.length; i++) {
+  //The i++ expression in programming is an example of an increment operation. It's shorthand for i = i + 1. What it does is increase the value of i by 1.
+  // This is a for loop, it will loop through the pokemonList array.
+  const pokemon = pokemonList[i];
+  const name = pokemon.name;
+  const height = pokemon.height;
+  const weight = pokemon.weight;
+  const types = pokemon.types;
+
+  let output = `${name} (height: ${height}) (weight: ${weight}) ${types} <br>`; // This specify what values will be displayed.
+
+  if (height > 6) {
+    // This is a conditional statement, if the height is greater than 6, it will display the message below.
+    output += " - Wow, that's big! <br>";
+  }
+
+  document.getElementById("output").innerHTML += output; // This is the output, allows the values to be displayed on the browser.
+}
